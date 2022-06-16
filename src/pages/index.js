51661634840d0807246467703cd8799898b914c6
Max McClaskie <max.mcclaskie@gmail.com>
2022-06-15 23:16:53 -0400

    changed order of last two sections

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,8 +19,8 @@ const IndexPage = () => {
         <Container>
             <Splash />
             <Streamline />
-            <HowItWorks />
             <Download />
+            <HowItWorks />
         </Container>
     );
 };
